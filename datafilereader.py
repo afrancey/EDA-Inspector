b072b4005579ac6a0950cb5ed7edd40110d5c9cf
afrancey <adam.francey.af@gmail.com>
2019-05-07 01:24:47 -0400

    delete unused file

--- datafilereader.py
+++
@@ -1,7 +0,0 @@
-# reads Empatica data files
-filepath = "C:\\Users\\alzfr\\Documents\\study\\data\\"
-filename = "EDA.csv"
-
-with open(filepath + filename, 'r') as f:
-    lines = f.readlines()
-    print(lines)
